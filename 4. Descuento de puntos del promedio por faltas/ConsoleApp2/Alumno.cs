85b56d47a6db37ef8328d773f68a812d12b10992
Juliana <152028539+ju-liana-cf@users.noreply.github.com>
2024-05-27 17:29:52 -0500

    Add files via upload

---
+++ 4. Descuento de puntos del promedio por faltas/ConsoleApp2/Alumno.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Alumno
+    {
+        public string nombre;
+        public double promedio;
+        public int numeroFaltas;
+
+        public void Datos() {
+            Console.WriteLine("Ingrese el nombre del alumno:");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el promedio del alumno:");
+            promedio = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el número de faltas del alumno:");
+            numeroFaltas = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+
+        public int CalcularPuntosARestar()
+        {
+            if (numeroFaltas == 0)
+            {
+                return 0;
+            }
+            else if (numeroFaltas >= 1 && numeroFaltas <= 3)
+            {
+                return 1;
+            }
+            else
+            {
+                return 2;
+            }
+        }
+
+        public void MostrarDatos()
+        {
+            Console.WriteLine("Nombre: " + nombre);
+            Console.WriteLine("Promedio actualizado: " + (promedio - CalcularPuntosARestar()));
+            Console.WriteLine("Número de faltas: " + numeroFaltas);
+        }
+    }
+}
---
+++ 4. Descuento de puntos del promedio por faltas/ConsoleApp2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Alumno alumno = new Alumno();
+
+
+                Console.WriteLine("-------------------------------------------");
+                Console.WriteLine("Datos del alumno:");
+                alumno.Datos();
+                alumno.CalcularPuntosARestar();
+                alumno.MostrarDatos();
+                Console.WriteLine("-------------------------------------------");
+
+                Console.WriteLine("¿Desea continuar con otro alumno? s/n");
+                string respuesta = Console.ReadLine().ToLower();
+
+                if (respuesta != "s")
+                {
+                    break;
+                }
+            } while (true);
+
+            Console.ReadKey();
+        }
+    }
+}

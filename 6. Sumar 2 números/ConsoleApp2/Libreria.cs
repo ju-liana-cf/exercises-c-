b012480308e580e4272e9939b8465b1cec6e9251
Juliana <152028539+ju-liana-cf@users.noreply.github.com>
2024-05-29 08:46:16 -0500

    Add files via upload

---
+++ 6. Sumar 2 números/ConsoleApp2/Libreria.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Libreria
+    {
+        public static void SaludoInicio(string nombre)
+        {
+            Console.WriteLine($"Hola {nombre}");
+        }
+
+        public static void sumar2Numeros(int a, int b)
+        {
+            int resultado = a + b;
+
+            Console.WriteLine($"El resultado es {resultado}");
+        }
+
+        public static void SaludoFin(string nombre)
+        {
+            Console.WriteLine($"Adios {nombre}");
+        }
+    }
+}
---
+++ 6. Sumar 2 números/ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Ingrese su nombre");
+                string nombre = Console.ReadLine();
+
+                Libreria.SaludoInicio(nombre);
+
+                Console.WriteLine("Ingrese primer número");
+                int a = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese segundo número");
+                int b = int.Parse(Console.ReadLine());
+
+                Libreria.sumar2Numeros(a, b);
+
+                Libreria.SaludoFin(nombre);
+
+                Console.WriteLine("¿Desea ingresar otros datos? s/n");
+                string r = Console.ReadLine().ToLower();
+
+                if (r != "s")
+                {
+                    break;
+                }
+
+            } while (true);
+
+            Console.ReadKey();
+        }
+    }
+}
